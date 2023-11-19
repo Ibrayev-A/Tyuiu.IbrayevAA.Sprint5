bf4761dd73ff8649ed84a1a60346cd7db60d82eb
Tyuiu.IbrayevAA@outlook.com <03.05.05.ibraev.abai@gmail.com>
2023-11-19 23:39:43 +0500

    Task2

---
+++ Tyuiu.IbrayevAA.Sprint5.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.IbrayevAA.Sprint5.Task2.V10.Lib;
+
+namespace Tyuiu.IbrayevAA.Sprint5.Task2.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Quyzt\Source\Repos\Tyuiu.IbrayevAA.Sprint5\Tyuiu.IbrayevAA.Sprint5.Task2.V10\bin\Debug\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.IbrayevAA.Sprint5.Task2.V10/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IbrayevAA.Sprint5.Task2.V10.Lib;
+
+namespace Tyuiu.IbrayevAA.Sprint5.Task2.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int stopValue = 5;
+            int startValue = -5;
+
+
+
+            Console.Title = "Спринт #5 | Выполнил: Ибраев А. А. | ПКТБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Ибраев Абай Аскарович | ПКТБ-23-2                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Введите {i}, {j} эллемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
