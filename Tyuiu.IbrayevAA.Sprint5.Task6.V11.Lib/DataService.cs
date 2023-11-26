2a29a28032fcc0f8eccbdb3f23e5d4b975078c19
Tyuiu.IbrayevAA@outlook.com <03.05.05.ibraev.abai@gmail.com>
2023-11-26 22:33:21 +0500

    Task6

---
+++ Tyuiu.IbrayevAA.Sprint5.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.IbrayevAA.Sprint5.Task6.V11.Lib
+{
+    public class DataService : ISprint5Task6V11
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                int x = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        x++;
+                        if ((line[i] == '.') || (line[i] == '?') || (line[i] == ',') || (line[i] == '!') || (line[i] == ' '))
+                        {
+                            if (x == 7)
+                            {
+                                count++;
+                                
+                            }
+                            x = 0;
+                        }
+                    }
+                }
+                //string line = reader.ReadToEnd();
+                //string[] linestr = line.Split();
+                //for (int i = 0; i < linestr.Length - 1; i++)
+                //{
+                //    if (linestr[i].Length == 6)
+                //    {
+                //        count++;
+                //    }
+                //}
+
+                return count;
+            }
+            
+        }
+    }
+}
---
+++ Tyuiu.IbrayevAA.Sprint5.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IbrayevAA.Sprint5.Task6.V11.Lib;
+using System.IO;
+
+namespace Tyuiu.IbrayevAA.Sprint5.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\Quyzt\Source\Repos\Tyuiu.IbrayevAA.Sprint5\Tyuiu.IbrayevAA.Sprint5.Task6.V11\bin\Debug\DataSprint5\InPutDataFileTask6V11.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+        public void TestMethod2()
+        {
+            string path = @"C:\Users\Quyzt\Source\Repos\Tyuiu.IbrayevAA.Sprint5\Tyuiu.IbrayevAA.Sprint5.Task6.V11\bin\Debug\DataSprint5\InPutDataFileTask6V11.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
